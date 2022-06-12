1648d336ae43e55cf10121ac799d111aca30fc37
Majiro <majiro1996@gmail.com>
2022-06-12 21:43:39 +0200

    Change operator event listener function to be able to string together several operations

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //variables
 let outputText="";
-let firstN
-let secondN
-let operator="+";
+let firstN;
+let secondN;
+let operator;
 let display=document.querySelector("#output");
+let push=0;
 //
 function add(a,b){
     outputText= a+b;
@@ -31,24 +32,36 @@ function operate(a,operator,b){
 let equal=document.querySelector("#equal");
 equal.addEventListener("click",()=>{
     secondN=Number(display.textContent);
-    console.log(secondN);
-    console.log(firstN);
-    console.log(operator);
+    // console.log(secondN);
+    // console.log(firstN);
+    // console.log(operator);
     operate(firstN,operator,secondN);
-    console.log(outputText);
+    // console.log(outputText);
     display.textContent=outputText;
-    secondN=0; 
+    secondN=0;
+    push=0;
 })
 
-let keys=document.querySelectorAll(".number");
-keys.forEach(key=>key.addEventListener("click",()=>{
+let numkeys=document.querySelectorAll(".number");
+numkeys.forEach(key=>key.addEventListener("click",()=>{
     outputText+=key.textContent;
     display.textContent=outputText;
 }))
 
-let opKey=document.querySelectorAll(".operator");
-opKey.forEach(op=>op.addEventListener("click",()=>{
-    firstN=Number(outputText);
-    operator=op.id;
-    outputText="";
+let opKeys=document.querySelectorAll(".operator");
+opKeys.forEach(op=>op.addEventListener("click",()=>{
+    if (push===0){
+        operator=op.id;
+        firstN=Number(outputText);
+        outputText="";
+        push=1;
+    }
+    else if (push===1){
+        secondN=Number(display.textContent);
+        operate(firstN,operator,secondN);
+        operator=op.id;
+        display.textContent=outputText;
+        firstN=outputText;
+        outputText="";
+    }
 }))
