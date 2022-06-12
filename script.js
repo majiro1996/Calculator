1332b82eb13c0cdd51ccb2b23d1401567b58dd9b
Majiro <majiro1996@gmail.com>
2022-06-12 17:46:49 +0200

    Add listeners to numbers, operators and equal buttons, and output for each calculation is displayed

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
+//variables
+let outputText="";
+let firstN
+let secondN
+let operator="+";
+let display=document.querySelector("#output");
 //
 function add(a,b){
-    let result=a+b;
-    return{result}
+    outputText= a+b;
 }
 function subtract(a,b){
-    let result=a-b;
-    return{result}
+    outputText= a-b;
 }
 function multiply(a,b){
-    let result=a*b;
-    return{result}
+    outputText= a*b;
 }
 function divide(a,b){
-    let result=a/b;
-    return {result}
+    outputText= a/b;
 }
 //
 function operate(a,operator,b){
-    if (operator==="+"){add(a,b)}
-    else if (operator==="-"){subtract(a,b)}
-    else if (operator==="*"){multiply(a,b)}
-    else if (operator==="/"){divide(a,b)}
+    if (operator==="add") {add(a,b);}
+    else if (operator==="subtract")  {subtract(a,b)}
+    else if (operator==="multiply")  {multiply(a,b)}
+    else if (operator==="divide")  {divide(a,b)}
+    else  {console.log("error");}
 }
+// operate(4,"+",4);
+// console.log(outputText);
+//
+let equal=document.querySelector("#equal");
+equal.addEventListener("click",()=>{
+    secondN=Number(display.textContent);
+    console.log(secondN);
+    console.log(firstN);
+    console.log(operator);
+    operate(firstN,operator,secondN);
+    console.log(outputText);
+    display.textContent=outputText;
+    secondN=0; 
+})
+
+let keys=document.querySelectorAll(".number");
+keys.forEach(key=>key.addEventListener("click",()=>{
+    outputText+=key.textContent;
+    display.textContent=outputText;
+}))
+
+let opKey=document.querySelectorAll(".operator");
+opKey.forEach(op=>op.addEventListener("click",()=>{
+    firstN=Number(outputText);
+    operator=op.id;
+    outputText="";
+}))
